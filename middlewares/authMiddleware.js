7f61120ceb922b0ff4eda7f3f2106fdbef500ece
laura.souza <laura.souza@gclaims.com.br>
2024-06-05 02:12:24 -0300

    ajuste validação do token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
+const admin = require('firebase-admin');
 
-// Middleware para validar o token JWT
-exports.verifyToken = (req, res, next) => {
+// Middleware para validar o token do Firebase
+exports.verifyFirebaseToken = (req, res, next) => {
   // Extrair o token do cabeçalho de autorização
   const authHeader = req.headers.authorization;
   if (!authHeader) {
@@ -14,14 +14,15 @@ exports.verifyToken = (req, res, next) => {
     return res.status(401).json({ error: 'Token inválido' });
   }
 
-  // Verificar e validar o token JWT
-  jwt.verify(token, 'secreto', (err, decodedToken) => {
-    if (err) {
-      return res.status(401).json({ error: 'Token inválido' });
-    } else {
+  // Verificar e validar o token do Firebase
+  admin.auth().verifyIdToken(token)
+    .then(decodedToken => {
       // Se o token for válido, você pode acessar as informações do usuário
-      req.userId = decodedToken.userId;
+      req.userId = decodedToken.uid;
       next();
-    }
-  });
-}
+    })
+    .catch(error => {
+      console.error('Erro ao verificar o token do Firebase:', error);
+      return res.status(401).json({ error: 'Token do Firebase inválido' });
+    });
+};
